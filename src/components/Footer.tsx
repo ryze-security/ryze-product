3684330d653c395eedf407e1c325c9ef50ee85c9
Aditya <devaditya491@gmail.com>
2025-06-28 20:40:49 +0530

    landing page is now mmobile compatible

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -20,65 +20,73 @@ function Footer(props: Props) {
 	return (
 		<div className="relative z-10 w-full font-roboto bg-[#0A0A0A]">
 			<Separator />
-			<section className="px-24 h-fit w-full pt-20 pb-8">
-				<div className="flex w-full justify-between px-3">
-					<div className="flex w-[60%]">
-						<div className="flex flex-col w-[50%]">
-							<h3 className="text-2xl leading-relaxed text-[#DDDDDD] mb-2">
+			<section className="px-4 md:px-12 xl:px-24 pt-20 pb-8 w-full">
+				<div className="flex flex-col lg:flex-row w-full justify-between gap-10">
+					{/* Product + Legal Links */}
+					<div className="flex flex-row w-full lg:w-[60%] gap-8">
+						{/* Product */}
+						<div className="flex flex-col w-full sm:w-[50%]">
+							<h3 className="text-xl md:text-2xl leading-relaxed text-[#DDDDDD] mb-2">
 								Product
 							</h3>
 							{ProductLinks.map((link, index) => (
 								<Button
 									key={index}
 									variant="link"
-									className="text-[#8A8A8A] w-fit text-left justify-start text-xl"
+									className="text-[#8A8A8A] w-fit text-left justify-start text-base md:text-xl"
 								>
 									{link.name}
 								</Button>
 							))}
 						</div>
-						<div className="flex flex-col w-[50%]">
-							<h3 className="text-2xl leading-relaxed text-[#DDDDDD] mb-2">
+
+						{/* Legal */}
+						<div className="flex flex-col w-full sm:w-[50%]">
+							<h3 className="text-xl md:text-2xl leading-relaxed text-[#DDDDDD] mb-2">
 								Legal
 							</h3>
 							{LegalLinks.map((link, index) => (
 								<Button
 									key={index}
 									variant="link"
-									className="text-[#8A8A8A] w-fit text-left justify-start text-xl"
+									className="text-[#8A8A8A] w-fit text-left justify-start text-base md:text-xl"
 								>
 									{link.name}
 								</Button>
 							))}
 						</div>
 					</div>
-					<div className="flex w-[40%] flex-col">
-						<h3 className="text-3xl leading-none text-[#DDDDDD] mb-2">
+
+					{/* Newsletter */}
+					<div className="w-full lg:w-fit xl:w-[40%] flex flex-col">
+						<h3 className="text-2xl md:text-3xl leading-none text-[#DDDDDD] mb-2">
 							Subscribe to Newsletter
 						</h3>
-						<p className="text-[#8A8A8A] text-lg">
+						<p className="text-[#8A8A8A] text-base md:text-lg">
 							Get Monthly insights on latest updates and security
 							tips.
 						</p>
-						<div className="bg-[#1E1E1E66] mt-5 flex justify-between rounded-full w-[453px] h-[55px] p-2">
+						<div className="bg-[#1E1E1E66] mt-5 flex flex-row justify-between rounded-full w-full md:w-full lg:w-[453px] md:h-fit h-[55px] p-2 gap-2 sm:gap-0">
 							<Input
 								type="email"
 								placeholder="Enter your email"
-								className="bg-transparent text-[#DDDDDD] w-[68%] p-0 rounded-full border-0 pl-5"
+								className="bg-transparent text-[#DDDDDD] w-full sm:w-[68%] p-0 rounded-full border-0 pl-5 h-12"
 							/>
-							<Button className="bg-[#DDDDDD] font-semibold text-[15px] text-[#0A0A0A] rounded-full w-[30%] h-full">
+							<Button className="bg-[#DDDDDD] font-semibold text-sm sm:text-[15px] text-[#0A0A0A] rounded-full w-[30%] h-12 ">
 								Subscribe
 							</Button>
 						</div>
 					</div>
 				</div>
-				<div className="flex justify-center h-[44px] mt-6 gap-6">
+
+				{/* Footer Bottom */}
+				<div className="flex flex-col sm:flex-row justify-center items-center h-fit mt-6 gap-4 sm:gap-6">
 					<img
-						src="\assets\Ryzr_White Logo_v2.png"
+						src="/assets/Ryzr_White Logo_v2.png"
 						alt="Ryzr Logo"
-						className="w-[37px] h-[37px]"
+						className="w-8 h-8"
 					/>
-					<h3 className="h-[44px] w-[290px] border-l pl-8 text-center leading-9 border-[#2A2A2A]">
+					<h3 className="sm:border-l pl-0 sm:pl-8 sm:border-[#2A2A2A] text-center text-sm sm:text-base text-[#AAAAAA]">
 						@2025 Ryzr. All rights reserved
 					</h3>
 				</div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,9 +13,6 @@ export default {
 		container: {
 			center: true,
 			padding: "2rem",
-			screens: {
-				"2xl": "1400px",
-			},
 		},
 		extend: {
 			colors: {
