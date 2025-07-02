81400d5263d6af83ce38abaeb9135f2d8b012d81
Aditya <devaditya491@gmail.com>
2025-07-02 17:05:35 +0530

    created dto and services for evaluations

---
+++ src/models/reports/ExcelDTOs.ts
@@ -0,0 +1,27 @@
+export interface createReportDTO {
+	tenant_id: string;
+	company_id: string;
+	evaluation_id: string;
+	report_type: string;
+	created_by: string;
+}
+
+export interface createStartReportResponseDTO {
+	report_id: string;
+	message: string;
+}
+
+export interface startReportDTO {
+	tenant_id: string;
+	company_id: string;
+}
+
+export interface reportResultDTO {
+	report_id: string;
+	eval_id: string;
+	report_type: string;
+	processing_status: string;
+	created_at: string;
+	created_by: string;
+	results: any[];
+}
---
+++ src/services/reportsServices.ts
@@ -0,0 +1,85 @@
+import {
+	createReportDTO,
+	createStartReportResponseDTO,
+	reportResultDTO,
+	startReportDTO,
+} from "@/models/reports/ExcelDTOs";
+import axios from "axios";
+import config from "./config";
+import { handleAxiosError } from "@/utils/handleAxiosError";
+
+export class ReportsService {
+	async createExcelReport(
+		userData: createReportDTO
+	): Promise<createStartReportResponseDTO | any> {
+		try {
+			const response = await axios.post<createStartReportResponseDTO>(
+				`${config.ryzrApiURL}/api/v1/`,
+				userData,
+				{
+					headers: {
+						"Content-Type": "application/json",
+						Accept: "application/json",
+					},
+				}
+			);
+			if (response.status !== 200) {
+				throw response;
+			}
+			return response.data;
+		} catch (error) {
+			const errorInfo = handleAxiosError(error);
+			console.error("Error creating excel report:", errorInfo.message);
+
+			//rethrowing for conditional rendering
+			throw errorInfo;
+		}
+	}
+
+	async startExcelReport(
+		report_id: string,
+		report_body: startReportDTO
+	): Promise<createStartReportResponseDTO | any> {
+		try {
+			const response = await axios.post<createStartReportResponseDTO>(
+				`${config.ryzrApiURL}/api/v1/${report_id}/start`,
+				report_body,
+				{
+					headers: {
+						"Content-Type": "application/json",
+						Accept: "application/json",
+					},
+				}
+			);
+			if (response.status !== 200) {
+				throw response;
+			}
+			return response.data;
+		} catch (error) {
+			const errorInfo = handleAxiosError(error);
+			console.error("Error creating excel report:", errorInfo.message);
+
+			//rethrowing for conditional rendering
+			throw errorInfo;
+		}
+	}
+
+    async getExcelReportResult(tenant_id: string,company_id: string, report_id: string): Promise<reportResultDTO|any> {
+        try{
+            const response = await axios.get<reportResultDTO>(
+                `${config.ryzrApiURL}/api/v1/${tenant_id}/${company_id}/${report_id}`
+            );
+            if (response.status !== 200) {
+                throw response;
+            }
+            return response.data;
+        }
+        catch (error) {
+            const errorInfo = handleAxiosError(error);
+            console.error("Error fetching report result:", errorInfo.message);
+
+            //rethrowing for conditional rendering
+            throw errorInfo;
+        }
+    }
+}
