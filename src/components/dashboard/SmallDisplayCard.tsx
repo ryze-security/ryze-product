4daf63032c9d3c299ed1b1838f3f3410a23cf676
Aditya <devaditya491@gmail.com>
2025-08-18 17:01:28 +0530

    added credits tracking to the dashboard

--- src/components/dashboard/SmallDisplayCard.tsx
+++ src/components/dashboard/SmallDisplayCard.tsx
@@ -11,19 +11,33 @@ interface Props {
 	title: string;
 	icon: React.ReactNode;
 	value: number;
-	percentageChange: number;
-	changeDescription: string;
+	percentageChange?: number;
+	changeDescription?: string;
+	warning?: boolean;
+	footer?: string;
 }
 
 function SmallDisplayCard(props: Props) {
-	const { title, icon, value, percentageChange, changeDescription } = props;
+	const {
+		title,
+		icon,
+		value,
+		percentageChange,
+		changeDescription,
+		warning = false,
+		footer,
+	} = props;
 
-    function formatNumberWithCommas(num: number | string): string {
-        return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-    }
+	function formatNumberWithCommas(num: number | string): string {
+		return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+	}
 
 	return (
-		<div className="bg-[#18181B] rounded-3xl flex flex-col w-[31%] min-h-[148px] justify-center gap-3 pl-6 py-5">
+		<div
+			className={`bg-[#18181B] rounded-3xl flex flex-col w-[96%] min-h-[148px] justify-start gap-3 pl-6 py-5 ${
+				warning && value === 0 ? "border-2 border-red-ryzr" : ""
+			}`}
+		>
 			<div className="text-gray-light-ryzr text-xl font-semibold tracking-wide flex gap-2">
 				{
 					<>
@@ -35,33 +49,40 @@ function SmallDisplayCard(props: Props) {
 				<span className="text-5xl font-semibold text-white">
 					{formatNumberWithCommas(value)}
 				</span>
-				<div
-					className={`rounded-full text-center flex items-center justify-center min-w-[62px] h-[26px] ${
-						changeDescription === "positive"
-							? "bg-green-ryzr"
-							: "bg-red-ryzr"
-					}`}
-				>
-					<TooltipProvider>
-						<Tooltip>
-							<TooltipTrigger>
-								<p className="text-sm">
-									{changeDescription === "positive"
-										? "+"
-										: "-"}
-									{percentageChange}%
-								</p>
-							</TooltipTrigger>
-							<TooltipContent className="bg-gray-ryzr/95">
-								<p>compared to last month</p>
-							</TooltipContent>
-						</Tooltip>
-					</TooltipProvider>
-				</div>
+				{changeDescription && percentageChange && (
+					<div
+						className={`rounded-full text-center flex items-center justify-center min-w-[62px] h-[26px] ${
+							changeDescription &&
+							changeDescription === "positive"
+								? "bg-green-ryzr"
+								: "bg-red-ryzr"
+						}`}
+					>
+						{changeDescription && percentageChange && (
+							<TooltipProvider>
+								<Tooltip>
+									<TooltipTrigger>
+										<p className="text-sm">
+											{changeDescription &&
+											changeDescription === "positive"
+												? "+"
+												: "-"}
+											{percentageChange}%
+										</p>
+									</TooltipTrigger>
+									<TooltipContent className="bg-gray-ryzr/95">
+										<p>compared to last month</p>
+									</TooltipContent>
+								</Tooltip>
+							</TooltipProvider>
+						)}
+					</div>
+				)}
 			</div>
+
 			<div>
 				<p className="font-light text-xs text-[#828282]">
-					compared to last month
+					{footer ? footer : "compared to last month"}
 				</p>
 			</div>
 		</div>
