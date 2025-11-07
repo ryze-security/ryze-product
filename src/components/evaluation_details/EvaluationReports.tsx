7dde11f7780cd6efbd6737e94bb7460734a35572
Aditya <devaditya491@gmail.com>
2025-11-07 18:41:16 +0530

    fixed markdown to tackle double *. added the the new bold algorithm to evaluation details report section and auditee evaluation table

--- src/components/evaluation_details/EvaluationReports.tsx
+++ src/components/evaluation_details/EvaluationReports.tsx
@@ -11,6 +11,9 @@ import * as ExcelJS from "exceljs";
 import * as FileSaver from "file-saver";
 import { GenericDataTable } from "../GenericDataTable";
 import { cn } from "@/lib/utils";
+import { createRichTextFromMarkdown } from "@/utils/markdownExcel";
+import companyService from "@/services/companyServices";
+import { CompanyListDto } from "@/models/company/companyDTOs";
 
 interface Props {
 	tenantId: string;
@@ -58,6 +61,7 @@ function EvaluationReports(props: Props) {
 	];
 
 	const [reports, setReports] = useState<ReportList[]>([]);
+	const [companyData, setCompanyData] = useState<CompanyListDto>(null);
 	const [isReportListLoading, setIsReportListLoading] =
 		useState<boolean>(false);
 	const [isReportDownloading, setIsReportDownloading] =
@@ -68,6 +72,10 @@ function EvaluationReports(props: Props) {
 		const fetchReports = async () => {
 			try {
 				setIsReportListLoading(true);
+				const companyDataResponse : CompanyListDto = await companyService.getCompanyByCompanyId(tenantId, companyId)
+				setCompanyData(companyDataResponse)
+
+
 				const response: reportResultListDTO =
 					await reportsService.getReportList(
 						tenantId,
@@ -113,116 +121,115 @@ function EvaluationReports(props: Props) {
 	};
 
 	const handleReportDownload = async (reportId: string) => {
-		try {
-			setIsReportDownloading(true);
-			const response: reportResultDTO =
-				await reportsService.getExcelReportResult(
-					tenantId,
-					companyId,
-					reportId
-				);
-			const df = new dfd.DataFrame(
-				response.results.sort((a, b) =>
-					a.control_id.localeCompare(b.control_id, undefined, {
-						numeric: true,
-					})
-				)
-			);
+        try {
+            setIsReportDownloading(true);
+            const response: reportResultDTO = await reportsService.getExcelReportResult(
+                tenantId,
+                companyId,
+                reportId
+            );
+            const df = new dfd.DataFrame(
+                response.results.sort((a, b) =>
+                    a.control_id.localeCompare(b.control_id, undefined, {
+                        numeric: true,
+                    })
+                )
+            );
 
-			const workbook = new ExcelJS.Workbook();
-			workbook.creator = "Ryzr";
-			workbook.created = new Date();
-			const worksheet = workbook.addWorksheet("Evaluation Report");
+            const workbook = new ExcelJS.Workbook();
+            workbook.creator = "Ryzr";
+            workbook.created = new Date();
+            const worksheet = workbook.addWorksheet("Evaluation Report");
 
-			const headerRow = worksheet.addRow(df.columns);
-			headerRow.eachCell((cell, index) => {
-				cell.value = formatHeaderCells(cell.value.toString());
-				cell.font = {
-					bold: true,
-					color: {
-						argb: "FFFFFFFF",
-					},
-					size: 12,
-					name: "SF Pro Display Regular",
-				};
-				cell.fill = {
-					type: "pattern",
-					pattern: "solid",
-					fgColor: {
-						argb: "FFB05AEF",
-					},
-				};
-				cell.alignment = {
-					vertical: "top",
-				};
-			});
+            const headerRow = worksheet.addRow(df.columns);
+            headerRow.eachCell((cell, index) => {
+                cell.value = formatHeaderCells(cell.value.toString());
+                cell.font = {
+                    bold: true,
+                    color: {
+                        argb: "FFFFFFFF",
+                    },
+                    size: 12,
+                    name: "SF Pro Display Regular",
+                };
+                cell.fill = {
+                    type: "pattern",
+                    pattern: "solid",
+                    fgColor: {
+                        argb: "FFB05AEF",
+                    },
+                };
+                cell.alignment = {
+                    vertical: "top",
+                };
+            });
+            const jsonData = dfd.toJSON(df) as Array<Record<string, any>>;
 
-			const jsonData = dfd.toJSON(df) as Array<Record<string, any>>;
+            //Adds and formats data rows
+            jsonData.forEach((record) => {
+                const row = worksheet.addRow(Object.values(record));
 
-			//Adds and formats data rows
-			jsonData.forEach((record) => {
-				const row = worksheet.addRow(Object.values(record));
+                row.eachCell((cell, index) => {
+                    if (index === 1) {
+                        const originalValue = cell.value ? cell.value.toString() : "";
 
-				row.eachCell((cell, index) => {
-					if (index === 1) {
-						const originalValue = cell.value
-							? cell.value.toString()
-							: "";
+                        cell.value = originalValue.slice(2);
+                    }
+                    cell.font = {
+                        size: 12,
+                        name: "SF Pro Display Regular",
+                        color: {
+                            argb: "FF000000",
+                        },
+                    };
+                    cell.alignment = {
+                        vertical: "top",
+                        wrapText: true,
+                    };
 
-						cell.value = originalValue.slice(2);
-					}
+                    if (cell.value && cell.value.toString().includes('*')) {
+                        cell.value = { richText: createRichTextFromMarkdown(cell.value.toString()) };
+                    }
+                });
+            });
 
-					cell.font = {
-						size: 12,
-						name: "SF Pro Display Regular",
-						color: {
-							argb: "FF000000",
-						},
-					};
-					cell.alignment = {
-						vertical: "top",
-						wrapText: true,
-					};
-				});
-			});
+            worksheet.columns.forEach((columns, index) => {
+                if (index <= 2) {
+                    columns.width = 25;
+                } else {
+                    columns.width = 50;
+                }
+                columns.border = {
+                    top: {
+                        style: "thin",
+                    },
+                    bottom: {
+                        style: "thin",
+                    },
+                    left: {
+                        style: "thin",
+                    },
+                    right: {
+                        style: "thin",
+                    },
+                };
+            });
 
-			worksheet.columns.forEach((columns, index) => {
-				if (index <= 2) {
-					columns.width = 25;
-				} else {
-					columns.width = 50;
-				}
-				columns.border = {
-					top: {
-						style: "thin",
-					},
-					bottom: {
-						style: "thin",
-					},
-					left: {
-						style: "thin",
-					},
-					right: {
-						style: "thin",
-					},
-				};
-			});
-
-			const buffer = await workbook.xlsx.writeBuffer();
-			const blob = new Blob([buffer], {
-				type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-			});
-			FileSaver.saveAs(blob, "report.xlsx");
-		} catch {
-			toast({
-				title: "Error",
-				description: `Failed to download report. Please try again later!`,
-				variant: "destructive",
-			});
-		} finally {
-			setIsReportDownloading(false);
-		}
-	};
+            const buffer = await workbook.xlsx.writeBuffer();
+            const blob = new Blob([buffer], {
+                type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+            });
+            FileSaver.saveAs(blob, companyData.tg_company_display_name + " report.xlsx");
+        } catch {
+            toast({
+                title: "Error",
+                description: `Failed to download report. Please try again later!`,
+                variant: "destructive",
+            });
+        } finally {
+            setIsReportDownloading(false);
+        }
+    };
 
 	return (
 		<div className="max-w-7xl w-full">
